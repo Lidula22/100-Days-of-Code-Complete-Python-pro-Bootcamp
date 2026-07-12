f1fd1157b14c83b1cf3a91f41d28d0543511d33a
Lidula22 <lidulaisiwara2002@gmail.com>
2026-07-12 22:34:57 +0530

    Create main.py
    Day 1: Band Name Generator

---
+++ Day-1-band-name-generator/main.py
@@ -0,0 +1,5 @@
+print("welcome to the band name generator")
+city=input("what is the city you were born in\n")
+pet_name=input("what is your pet name\n")
+print("your band name is "+city +" "+pet_name)
+
